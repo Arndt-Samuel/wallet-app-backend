f2a6e00d315d67c51066e8fe4a8f73038e96841f
Samuel Arndt <samuelarndt@hotmail.com>
2023-10-02 14:10:11 -0300

    initial commit

---
+++ src/db.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const { Pool } = require('pg');
+
+const { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT } = process.env
+
+const db = new Pool({
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    port: Number(DB_PORT),
+});
+
+module.exports = db;
---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const db = require('./db');
+const routesCategories = require('./routes/categories');
+const routesUser = require('./routes/users');
+const routesFinances = require('./routes/finances');
+const app = express()
+app.use(express.json())
+
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use('/categories', routesCategories);
+app.use('/user', routesUser);
+app.use('/finances', routesFinances);
+
+app.listen(port, () => {
+  db.connect().then(() => {
+    console.log('DB connected!');
+  }).catch(error => {
+    throw new Error(error);
+  })
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+
+
